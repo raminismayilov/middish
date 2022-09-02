1e9e80fdd389902c3f5a6240fe59f4b731833a59
Ramin Ismayilov <ramismayilov@gmail.com>
2022-09-02 19:54:06 +0400

    Initial commit

---
+++ test/middish.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Middish from '../lib/middish-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/middish-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Middish.MiddishStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
